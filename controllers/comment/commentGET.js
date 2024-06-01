6f75c113c6df152d3ac9378551cd41e1235457ea
senzoo-oh <osj3418@gmail.com>
2024-06-01 14:18:48 +0900

    refactor: 🔨댓글조회 수정

--- controllers/comment/commentGET.js
+++ controllers/comment/commentGET.js
@@ -1,14 +1,17 @@
 const responseMessage = require("../../constants/responseMessage");
 const statusCode = require("../../constants/statusCode");
 const util = require("../../lib/util");
+const userController = require("../user/userNicknameGET");
 const { commentDB } = require("../../models");
 
 module.exports = async (req, res) => { // req에 요청값이 담긴다.
     try {
         const {cHistoryID} = req.params; // {} 안의 것을 req에서 뽑아온다.
-        const result = await commentDB.getComment(cHistoryID);
+        const result = await commentDB.getComment(cHistoryID);  // comment에 대한 정보 얻기\
+        result.nickname = await userController(result.userId);
 
-        return res.status(statusCode.OK)
+        
+        return res.status(statusCode.OK)    // 뷰로 result보냄
         .send(util.success(statusCode.OK, responseMessage.READ_ALL_COMMENT_SUCCESS, result));
     }
     catch(err) {
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const commentGET = require('../controllers/comment/commentGET');
+const commentPOST = require('../controllers/comment/commentPOST');
+const commentDELETE = require('../controllers/comment/commentDELETE');
+const commentEDIT = require('../controllers/comment/commentEDIT');
+
+
 const router = express.Router();
 
-//router.use('/mycalendar', require('./mycalendar'));
-//router.use('/consumptions',require('./consumptionHistory')); 
-router.use('/comment', require('./comment'));
-//router.use('/calendarfeed',require('./calendarFeed'));
-//router.use('/friends', require('./friends'));
-//router.use('/emoticon', require('./emoticon'));
-//router.use('/user',require('./user'));
-//router.use('/auth', require('./auth'));
+router.get('/:cHistoryID', commentGET); // 전체 댓글 보기
+router.post('/:cHistoryID', commentPOST); // 댓글 작성
+router.put('/:cHistoryID/:commentID', commentEDIT); // 댓글 수정
+router.put('/:cHistoryID/:commentID', commentDELETE); // 댓글 삭제
+
 module.exports = router;
