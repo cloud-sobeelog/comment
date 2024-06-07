444e0e2a6b8b1e2c9f7dcc68fd3e137f341c2fb1
senzoo-oh <osj3418@gmail.com>
2024-06-08 01:34:06 +0900

    refactor: 🔨라우터 순서 변경

--- routes/comment/index.js
+++ routes/comment/index.js
@@ -7,11 +7,12 @@ const countOfCommentGET = require('../../controllers/comment/countOfCommentGET')
 
 const router = express.Router();
 
+router.get('/count/:cHistoryID', countOfCommentGET);
 router.get('/:cHistoryID', commentGET); // 전체 댓글 보기
 router.post('/:cHistoryID', commentPOST); // 댓글 작성
 router.put('/:cHistoryID/:commentID', commentEDIT); // 댓글 수정
 router.delete('/:cHistoryID/:commentID', commentDELETE); // 댓글 삭제
-router.get('/count/:cHistoryID', countOfCommentGET);
+
 
 //router.put('/:cHistoryID/:commentID', commentDELETE); // 댓글 삭제
 
