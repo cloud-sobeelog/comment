25420d768b7131f0c6fde9c9b109a451ae860eb5
choiyoorim <yulim3839@naver.com>
2024-06-14 02:50:50 +0900

    refactor: 🔨 comment GET 병렬처리로 수정

--- controllers/comment/commentGET.js
+++ controllers/comment/commentGET.js
@@ -1,43 +1,37 @@
 const responseMessage = require("../../constants/responseMessage");
 const statusCode = require("../../constants/statusCode");
 const util = require("../../lib/util");
-const axios = require('axios');
 
 const { commentDB } = require("../../models");
 const { client } = require("../../lib/api");
 
-
-module.exports = async (req, res) => { // req에 요청값이 담긴다.
+module.exports = async (req, res) => {
     try {
-        const {cHistoryID} = req.params; // {} 안의 것을 req에서 뽑아온다.
-        const result = await commentDB.getComment(cHistoryID);  // comment에 대한 정보 얻기
+        const { cHistoryID } = req.params;
+        const result = await commentDB.getComment(cHistoryID);
         console.log(result);
-        
-        // 댓글이 없는 경우
-        if (result.length == 0) {
+
+        if (result.length === 0) {
             return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ALL_COMMENT_SUCCESS, result));
         }
-        
-        for (var i = 0; i < result.length; i++) {
-            // console.log(result[i]);
-            // console.log(result[i].userID);
-            const userID = result[i].userID;
 
-            const { data } = await client.get(`/user/userInfo/${userID}`);
-            result[i].nickname = data.data[0].nickname;
-            //console.log(findData.commentID);
-        }
+        // 모든 사용자 ID를 배열로 추출
+        const userIDs = result.map(comment => comment.userID);
 
-        //console.log(data);
-        //console.log(data.data[0]);
-        //console.log(data.data[0].userID);
-        //console.log(data.data[0].nickname);
-        
-        return res.status(statusCode.OK)    // 뷰로 result보냄
-        .send(util.success(statusCode.OK, responseMessage.READ_ALL_COMMENT_SUCCESS, result));
-    }
-    catch(err) {
+        // 병렬로 사용자 정보를 가져오기
+        const userRequests = userIDs.map(userID => client.get(`/user/userInfo/${userID}`));
+        const userResponses = await Promise.all(userRequests);
+
+        // 사용자 정보를 결과에 병합
+        userResponses.forEach((response, index) => {
+            const nickname = response.data.data[0].nickname;
+            result[index].nickname = nickname;
+        });
+
+        console.log(result);
+        return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ALL_COMMENT_SUCCESS, result));
+    } catch (err) {
         console.log(err);
         return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
     }
-}
+};
